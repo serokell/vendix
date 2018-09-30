bd7a0fd9bbdc3c2e15e9130be90d54c03acb0696
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-30 15:57:19 +0000

    Run basic test during Nix build

--- default.nix
+++ default.nix
@@ -4,12 +4,13 @@ stdenv.mkDerivation {
   name = "nix-vendor";
   src = lib.cleanSource ./.;
 
-  nativeBuildInputs = [ meson ninja ];
+  nativeBuildInputs = [ coreutils meson ninja ];
 
   doCheck = true;
 
-  preCheck = ''
-    cp -r ../test/nix .
-    ls -ahl
+  checkPhase = ''
+    test "59hb78ss84bkgqvqyyn4z5s6qmlxpp57-hello" = "$(env \
+      DYLD_INSERT_LIBRARIES=$PWD/libnixvendor.dylib \
+      DYLD_ROOT_PATH=$PWD/../tests ls /nix/store)"
   '';
 }
